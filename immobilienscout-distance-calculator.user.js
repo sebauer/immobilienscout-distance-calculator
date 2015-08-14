ea619f1a7bea831f8738611eb7c437546546d7cf
Sebastian Bauer <gjlnetwork@gmail.com>
2015-08-14 10:20:49 +0200

    Reformatted according to Semi-Standard style

--- immobilienscout-distance-calculator.user.js
+++ immobilienscout-distance-calculator.user.js
@@ -9,65 +9,64 @@
 // @match        http://www.immobilienscout24.de/expose/*
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
-function sendMapsRequest(origin, destination, targetElem) { 
-    var req = new GM_xmlhttpRequest({
-        method: 'GET',
-        url: 'https://maps.googleapis.com/maps/api/directions/json?origin='+encodeURIComponent(origin)+'&destination='+encodeURIComponent(destination)+'&key=AIzaSyBnRL5RMvFyq1QjGZRQPuhrrSLgpICJyXc',
-        onload: function(res) {
-            
-            console.log(res);
-            
-            var resObj = JSON.parse(res.responseText);
-            var resultRow = document.createElement('div');
-            resultRow.innerHTML = origin+': '+resObj.routes[0].legs[0].distance.text + ' / '+ resObj.routes[0].legs[0].duration.text;
-            targetElem.appendChild(resultRow);
-        }
-    });
-}
-
-function calculateGoogleMapsRoutes(elem){
-    var destination = elem.getAttribute('data-destination');
-    
-    var header = elem.parentNode;
-    var routesDiv = document.createElement('div');
-    routesDiv.setAttribute('class', 'font-s');
-    header.appendChild(routesDiv);
-    
-    sendMapsRequest('Köln', destination, routesDiv);
-    sendMapsRequest('Bonn', destination, routesDiv);
-}
-
-function addRouteButton(elem, destination) {
-    var button = document.createElement('button');
-    button.setAttribute('style', 'display: block;');
-    button.setAttribute('data-destination', destination);
-    button.onclick = function(){
-        this.style.display = 'none';
-        calculateGoogleMapsRoutes(this);
-    };
-    button.innerHTML = 'Berechnen';
-    elem.parentNode.parentNode.appendChild(button);
-}
-
-function getResultElements() {
-    return document.evaluate( "/html/body//span[contains(@class, 'street')]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE , null );
-}
-
-window.addEventListener('load', function() {
-    console.log('run');
-    var resultSet = getResultElements();
-    console.log(resultSet);
-    var currentResult = null;
-    for ( var i=0 ; i < resultSet.snapshotLength; i++ ) {
-        currentResult = resultSet.snapshotItem(i);
-        addRouteButton(currentResult, currentResult.innerHTML);
+/* global GM_xmlhttpRequest, XPathResult */
+function sendMapsRequest (origin, destination, targetElem) {
+  GM_xmlhttpRequest({
+    method: 'GET',
+    url: 'https://maps.googleapis.com/maps/api/directions/json?origin=' + encodeURIComponent(origin) + '&destination=' + encodeURIComponent(destination) + '&key=AIzaSyBnRL5RMvFyq1QjGZRQPuhrrSLgpICJyXc',
+    onload: function (res) {
+      console.log(res);
+      var resObj = JSON.parse(res.responseText);
+      var resultRow = document.createElement('div');
+      resultRow.innerHTML = origin + ': ' + resObj.routes[0].legs[0].distance.text + ' / ' + resObj.routes[0].legs[0].duration.text;
+      targetElem.appendChild(resultRow);
     }
-    
-    // is24-expose-address
-    var addressElemOnExpose = document.evaluate( "/html/body//span[@data-qa='is24-expose-address']", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE , null );
-    if(addressElemOnExpose.snapshotLength > 0) {
-        addRouteButton(addressElemOnExpose.snapshotItem(0), addressElemOnExpose.snapshotItem(0).textContent);
-    }
-    
-    console.log('done');
+  });
+}
+
+function calculateGoogleMapsRoutes (elem) {
+  var destination = elem.getAttribute('data-destination');
+
+  var header = elem.parentNode;
+  var routesDiv = document.createElement('div');
+  routesDiv.setAttribute('class', 'font-s');
+  header.appendChild(routesDiv);
+
+  sendMapsRequest('Köln', destination, routesDiv);
+  sendMapsRequest('Bonn', destination, routesDiv);
+}
+
+function addRouteButton (elem, destination) {
+  var button = document.createElement('button');
+  button.setAttribute('style', 'display: block;');
+  button.setAttribute('data-destination', destination);
+  button.onclick = function () {
+    this.style.display = 'none';
+    calculateGoogleMapsRoutes(this);
+  };
+  button.innerHTML = 'Berechnen';
+  elem.parentNode.parentNode.appendChild(button);
+}
+
+function getResultElements () {
+  return document.evaluate("/html/body//span[contains(@class, 'street')]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+}
+
+window.addEventListener('load', function () {
+  console.log('run');
+  var resultSet = getResultElements();
+  console.log(resultSet);
+  var currentResult = null;
+  for (var i = 0; i < resultSet.snapshotLength; i++) {
+    currentResult = resultSet.snapshotItem(i);
+    addRouteButton(currentResult, currentResult.innerHTML);
+  }
+
+  // is24-expose-address
+  var addressElemOnExpose = document.evaluate("/html/body//span[@data-qa='is24-expose-address']", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+  if (addressElemOnExpose.snapshotLength > 0) {
+    addRouteButton(addressElemOnExpose.snapshotItem(0), addressElemOnExpose.snapshotItem(0).textContent);
+  }
+
+  console.log('done');
 }, false);
